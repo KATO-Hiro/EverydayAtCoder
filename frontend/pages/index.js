5e1fb30ae88990f179899b2a07ea4c76d51b3a36
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-08 00:51:20 +0000

    Add link of about page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
+import Link from "next/link";
 
 export default function Index() {
   return (
     <div>
       <p>Hello, Next.js</p>
+
+      <Link href="/about">
+        <a title="About Page">About Page</a>
+      </Link>
     </div>
   );
 }
