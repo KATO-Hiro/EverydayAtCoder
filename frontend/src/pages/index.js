6bde46020118cc391209ac54cc2cb15b3cb209b8
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-08 07:53:02 +0000

    Create dynamic pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import Layout from "../components/MyLayout";
 
-const indexPageContent = <p>Hello Next.js</p>;
+const indexPageContent = <h1>My Blog</h1>;
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 export default function Index() {
   return (
     <div>
       <Layout content={indexPageContent} />
+      <ul>
+        <PostLink title="Hello Next.js"/>
+        <PostLink title="Learn Next.js is awesome"/>
+        <PostLink title="Deploy apps with Zeit"/>
+      </ul>
     </div>
   );
 }
