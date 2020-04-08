44b3f8b383ecfe8e8d9e24cd91f559e3ebbad9c0
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-08 13:27:26 +0000

    Enable to fetch public API

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,11 +1,33 @@
 import Layout from "../components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const aboutPageContent = <p>This is the about page.</p>;
+const aboutPageContent = <h1>Batman TV Shows</h1>;
 
-export default function About() {
-  return (
-    <div>
-      <Layout content={aboutPageContent} />
-    </div>
-  );
-}
+const About = props => (
+  <div>
+    <Layout content={aboutPageContent} />
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+About.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default About;
