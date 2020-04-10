d74b72d15d85d40876520d397b9ebbfe5deadbcc
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-10 22:43:31 +0000

    Enable to show a simplest table (#39)

--- frontend/src/pages/history.js
+++ frontend/src/pages/history.js
@@ -1,9 +1,103 @@
+import React from 'react';
+import { useTable } from "react-table";
 import Layout from "../components/MyLayout";
 
 const histroyPageContent = <h1>History</h1>;
 
-export default function History() {
+// TODO: Extract a component.
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data
+  })
+
+  // Render the UI for your table
   return (
-    <Layout content={histroyPageContent} />
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  border: 'solid 1px gray'
+                }}
+              >
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row)
+
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (<td
+                         {...cell.getCellProps()}
+                         style={{
+                           border: 'solid 1px gray',
+                         }}
+                       >
+                         {cell.render('Cell')}
+                       </td>);
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default function History() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Hoge',
+        accessor: 'col1',
+      },
+      {
+        Header: 'Foo',
+        accessor: 'col2',
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'Hello',
+        col2: 'World',
+      },
+      {
+        col1: 'react-table',
+        col2: 'rocks',
+      },
+      {
+        col1: 'whatever',
+        col2: 'you want',
+      },
+    ],
+    []
+  );
+
+  return (
+    <div>
+      <Layout content={histroyPageContent} />
+      <Table columns={columns} data={data} />
+    </div>
   );
 }
