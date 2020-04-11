9e7df84f9e13b8e3dc18d599cc0e1f92a163a25d
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-11 04:26:26 +0000

    Extract a table as a component

--- frontend/src/pages/history.js
+++ frontend/src/pages/history.js
@@ -1,65 +1,9 @@
 import React from 'react';
 import { useTable } from "react-table";
 import Layout from "../components/MyLayout";
+import ContestHistoryTable from "../components/ContestHistoryTable";
 
 const histroyPageContent = <h1>History</h1>;
-
-// TODO: Extract a component.
-function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
-    columns,
-    data
-  })
-
-  // Render the UI for your table
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  border: 'solid 1px gray'
-                }}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (<td
-                         {...cell.getCellProps()}
-                         style={{
-                           border: 'solid 1px gray',
-                         }}
-                       >
-                         {cell.render('Cell')}
-                       </td>);
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
-}
 
 export default function History() {
   const columns = React.useMemo(
@@ -118,7 +62,7 @@ export default function History() {
   return (
     <div>
       <Layout content={histroyPageContent} />
-      <Table columns={columns} data={data} />
+      <ContestHistoryTable columns={columns} data={data} />
     </div>
   );
 }
