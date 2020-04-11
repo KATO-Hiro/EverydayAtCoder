efffb85d03b4282ffcf5c2d0e2d7d6febcbbd3ce
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-11 07:55:53 +0000

    Add pagination to History page (#44)

--- frontend/src/components/ContestHistoryTable.js
+++ frontend/src/components/ContestHistoryTable.js
@@ -1,4 +1,4 @@
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
 
 export default function ContestHistoryTable({ columns, data }) {
   // Use the state and functions returned from useTable to build your UI
@@ -6,52 +6,106 @@ export default function ContestHistoryTable({ columns, data }) {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
   } = useTable({
-    columns,
-    data
-  })
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    usePagination
+  )
 
   // Render the UI for your table
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  border: 'solid 1px gray'
-                }}
-              >
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
+    <>
+      <select
+        value={pageSize}
+        onChange={e => {
+          setPageSize(Number(e.target.value))
+        }}
+      >
+        {[10, 20, 50, 100].map(pageSize => (
+          <option key={pageSize} value={pageSize}>
+            Show {pageSize} contests / page
+          </option>
         ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
+      </select>
 
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (<td
-                         {...cell.getCellProps()}
-                         style={{
-                           border: 'solid 1px gray',
-                         }}
-                       >
-                         {cell.render('Cell')}
-                       </td>);
-              })}
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    border: 'solid 1px gray'
+                  }}
+                >
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (<td
+                          {...cell.getCellProps()}
+                          style={{
+                            border: 'solid 1px gray',
+                          }}
+                        >
+                          {cell.render('Cell')}
+                        </td>);
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+
+        <span>
+          Page: {' '}
+          <strong>
+            {pageIndex + 1} / {pageOptions.length}
+          </strong>
+        </span>{' '}
+
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+
+      </div>
+    </>
   )
 }
