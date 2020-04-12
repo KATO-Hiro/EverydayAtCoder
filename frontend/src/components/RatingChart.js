e36a5171e7db726ce15fa380eed99538c253db13
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-12 01:47:06 +0000

    Add rating colors to rating chart (#57)

--- frontend/src/components/RatingChart.js
+++ frontend/src/components/RatingChart.js
@@ -1,12 +1,20 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
 import GenerateData from '../components/GenerateData';
+import {
+  ratingColors
+} from "../utils/ratingColors";
 
 const data = GenerateData(100);
 
 const renderLineChart = (
   <LineChart width={600} height={350} data={data}>
     <Line type="monotone" dataKey="NewRating" stroke="#8884d8"></Line>
-    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <CartesianGrid
+      stroke="#ccc"
+      strokeDasharray="5 5"
+      horizontalFill={ratingColors}
+      fillOpacity={0.2}
+    />
     <XAxis dataKey="" />
     <YAxis
       domain={[dataMin => Math.max(0, dataMin - 200), 'dataMax + 200']}
