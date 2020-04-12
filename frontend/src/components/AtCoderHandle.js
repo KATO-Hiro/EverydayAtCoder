de1e746e5aeeda7e6f50aed1c07abf1c64966ba3
Kato Hiroki <k.hiro1818@gmail.com>
2020-04-12 10:32:43 +0000

    Enable to show dummy user name (#69)

---
+++ frontend/src/components/AtCoderHandle.js
@@ -0,0 +1,26 @@
+import ratingColorStyle from "../utils/ratingColorStyle";
+
+// See:
+// https://greasyfork.org/ja/scripts/397710-atcoder-submission-user-colorizer/code
+// TODO: Enable to change color and ach rate.
+// TODO: Extract a component.
+const style = {
+  display: 'inline-block',
+  width: '16px',
+  height: '16px',
+  borderRadius: '50%',
+  verticalAlign: 'center',
+  border: '1px solid red',
+  background: '-webkit-linear-gradient(bottom, red 0%, red 100%, rgba(255, 255, 255, 0.0) 100%, rgba(255, 255, 255, 0.0) 100%)',
+};
+
+const AtCoderHandle = (props) => (
+  <div className="atcoder-handle" align="center">
+    <h1 style={ratingColorStyle(props.currentRating)}>
+      <span style={style}></span>
+      {props.handleName}
+    </h1>
+  </div>
+);
+
+export default AtCoderHandle;
