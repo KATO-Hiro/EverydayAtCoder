7acdeb353b7f64fbc64e6bbd9f997449ca143d05
Kato Hiroki <k.hiro1818@gmail.com>
2020-05-13 07:34:26 +0000

    Add setting files for Firebase

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,34 @@
 // https://qiita.com/syuji-higa/items/931e44046c17f53b432b#1-1-nextjs-%E3%81%AE%E8%A8%AD%E5%AE%9A
 const { resolve } = require('path');
 
+// See:
+// https://github.com/benzguo/nextjs-now-firebase/blob/master/next.config.js
+require("dotenv").config();
+
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias['~'] = resolve(__dirname, 'src')
 
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty"
+      };
+    }
+
     return config
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY
   }
 };
 
