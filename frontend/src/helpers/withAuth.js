cbeecb8191991115ffd6af11090e71c431a4ebe4
Kato Hiroki <k.hiro1818@gmail.com>
2020-05-27 13:26:25 +0000

    rewrite HOC using hooks

--- frontend/src/helpers/withAuth.js
+++ frontend/src/helpers/withAuth.js
@@ -1,53 +1,42 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 import router from 'next/router';
 import {
   auth,
 } from '../firebase';
+import { colors } from '@material-ui/core';
 
 // See:
 // https://medium.com/@uvictor/simple-firebase-authentication-with-next-js-using-hoc-higher-order-components-8e8931d25cfa
 // https://nekorokkekun.hatenablog.com/entry/2019/09/25/000041#%E3%82%B5%E3%82%A4%E3%83%B3%E3%82%A4%E3%83%B3%E3%82%92%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B
-const withAuth = (Component) => {
-  // TODO: Rewrite in function format.
-  return (
-    class extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          status: "LOADING"
-        };
-      }
+// https://medium.com/javascript-scene/do-react-hooks-replace-higher-order-components-hocs-7ae4a08b7b58
+const withAuth = (Component) => props => {
+  const [status, setStatus] = useState("LOADING");
 
-      componentDidMount() {
-        auth.onAuthStateChanged(authUser => {
-          if (authUser) {
-            this.setState({
-              status: "SIGNED_IN"
-            });
-          } else {
-            router.push("/");
-          }
-        });
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        setStatus("SIGNED_IN");
+      } else {
+        router.push("/");
       }
+    });
+  }, [status]);
 
-      renderContent() {
-        const { status } = this.state;
-
-        if (status == "LOADING") {
-          return <h1>Loading ......</h1>;
-        } else if (status == "SIGNED_IN") {
-          return <Component { ...this.props } />;
-        }
-      }
-
-      render() {
-        return (
-          <>
-            {this.renderContent()}
-          </>
-        );
-      }
+  function renderContent() {
+    if (status == "LOADING") {
+      return <h1>Loading ......</h1>;
+    } else if (status == "SIGNED_IN") {
+      return <Component { ...props } />;
     }
+  }
+
+  return (
+    <>
+      { renderContent() }
+    </>
   );
 };
 
